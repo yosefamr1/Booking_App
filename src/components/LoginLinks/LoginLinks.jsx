4d7aa10e944501995e98fb4e358c4788d6d5ac82
Youssef's Acer <yaamr2002@gmail.com>
2025-07-31 21:33:36 +0300

    nav links update

--- src/components/LoginLinks/LoginLinks.jsx
+++ src/components/LoginLinks/LoginLinks.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function LoginLinks() {
   return (
-    <div >
-            <a href="/login" className="text-lg  text-white hover:underline">Login</a>
-            <a href="/register" className="text-lg  text-white hover:underline pl-2">Sign up</a>
+    <div className='flex'>
+      <Link to={`/login`}>
+        <p className="text-lg  text-white hover:underline pl-2">Login</p>
+      </Link>
+      <Link to={`/register`}>
+        <p className="text-lg  text-white hover:underline pl-2">sign up</p>
+      </Link>
     </div>
   )
 }
